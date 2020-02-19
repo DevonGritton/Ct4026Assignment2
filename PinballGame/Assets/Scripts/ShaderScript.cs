a1f24146b845e7642c1441994c6fec41c8d31eee
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2020-02-19 12:42:40 +0000

    Added Shader
    Added shader for the pinball when it falls back into the machine it starts to dissolve then deltes itself

---
+++ PinballGame/Assets/Scripts/ShaderScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderScript : MonoBehaviour
+{
+    [SerializeField]
+    public Material dissolveMat;
+    public float MaxHealth = 3f;
+    public float Health = -1f;
+    public bool death;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dissolveMat.SetFloat("Vector1_C42086C0", -1);
+        Health = -1f;
+        death = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Killbox")
+        {
+            death = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (death)
+        {
+            dissolveMat.SetFloat("Vector1_C42086C0", Health);
+            Health = Time.deltaTime + 0.1f;
+            MaxHealth -= Time.deltaTime;
+            if (MaxHealth <= 0)
+            {
+                Destroy(gameObject);
+                dissolveMat.SetFloat("Vector1_C42086C0", -1);
+                death = false;
+            }
+        }
+    }
+}
