4bcf7b912e2043a33256ec25cbcbbc13961a4656
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2020-03-02 16:29:18 +0000

    Updated level
    Updated level design and plunger functionality as well as tidied up menus and scripts

--- PinballGame/Assets/Scripts/HoleScript.cs
+++ PinballGame/Assets/Scripts/HoleScript.cs
@@ -4,26 +4,15 @@ using UnityEngine;
 
 public class HoleScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Ball")
         {
-            Destroy(other.gameObject);
             Health.HpValue -= 1;
         }
         else
         {
             return;
         }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
---
+++ PinballGame/Assets/Scripts/Plunger.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Plunger : MonoBehaviour
+{
+    float power;
+    float minPower = 0f;
+    public float maxPower = 10000f;
+    public Slider plungerSlider;
+    List<Rigidbody> ballList;
+    bool ballReady;
+    void Start()
+    {
+        ballReady = false;
+        plungerSlider.minValue = 0;
+        plungerSlider.maxValue = maxPower;
+        //plungerSlider.enabled = true;
+        ballList = new List<Rigidbody>();
+    }
+    void Update()
+    {
+        plungerSlider.value = power;
+        if (ballList.Count >= 0)
+        {
+            ballReady = true;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if(power <= maxPower)
+                {
+                    power += 500 * Time.deltaTime;
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                foreach(Rigidbody r in ballList)
+                {
+                    r.AddForce(power * Vector3.forward);
+                }
+            }
+        }
+        else
+        {
+            ballReady = false;
+            power = 0f;
+        }
+    }
+
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            plungerSlider.gameObject.SetActive(true);
+            ballList.Add(other.gameObject.GetComponent<Rigidbody>());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            plungerSlider.gameObject.SetActive(false);
+            ballList.Remove(other.gameObject.GetComponent<Rigidbody>());
+            power = 0f;
+        }
+    }
+}
